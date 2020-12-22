4a9076826178950c446aa4ee89d56a3643f5cd64
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-12-22 22:05:36 +0530

    Update Video.js

--- src/Video.js
+++ src/Video.js
@@ -20,8 +20,7 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://server.meet.logyc.xyz' : "http://localhost:4001"
-
+const server_url = process.env.NODE_ENV === 'production' ? 'https://video.sebastienbiollo.com' : "http://localhost:4001"
 var connections = {}
 const peerConnectionConfig = {
 	'iceServers': [
