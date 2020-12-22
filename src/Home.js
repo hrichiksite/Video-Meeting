92ae8e0333f0c9be213be903e58f29b3909eaf25
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-12-22 15:00:42 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -24,27 +24,24 @@ class Home extends Component {
 	}
 
 	render() {
-		return (	
-						<div className="container2">		
+		return (
 			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>					<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
-					Source code: 						Source code: 
-					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>						<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
-						<GitHubIcon />							<GitHubIcon />
-					</IconButton>						
-                                   </IconButton>
+				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
+					Source code: 
+					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
+						<GitHubIcon />
+					</IconButton>
 				</div>
-			
+				
 				<div>
 					<h1 style={{ fontSize: "45px" }}>MeetUp Now</h1>
 					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends. Unlimited Users, Screen Share, Meet Chat and more. All For Free.</p>
-                                </div>
+				</div>
 
 				<div style={{
 					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
 					textAlign: "center", margin: "auto", marginTop: "100px"
 				}}>
-					<img src="https://i.imgur.com/mrITChb.png" alt="Meeting" width="200" height="150">
 					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
 					<Input placeholder="Meet Name or ID" onChange={e => this.handleChange(e)} />
 					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
